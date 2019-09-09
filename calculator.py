914feb37022b36d8f3f8fb80531236a46dfd61f4
Samuel Li <SamuelLi@Samuels-MacBook-Pro-2.local>
2019-09-09 12:22:58 -0400

    wrote function to analyze HDL test result

--- calculator.py
+++ calculator.py
@@ -1,13 +1,22 @@
+def check_HDL(HDL_result):
+	if HDL_result >= 60:
+		return "Normal"
+	elif 40 <= HDL <60:
+		return "Bordline low"
+	else:
+		return "low"
+
+
 def interface():
 	print("My calculator program")
         keep_running = True
-        while keep_running:
-	        print("Option: ")
-	        print("0 - Quit")
-	        choice = input("Enter your choice: ")
-	        if choice == '9':
-		        return
-        return
+	        while keep_running:
+		        print("Option: ")
+		        print("0 - Quit")
+		        choice = input("Enter your choice: ")
+		        if choice == '9':
+			        return
+	        return
 
 if __name__ == "__main__":
 	interface()
