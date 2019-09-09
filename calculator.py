b1cc7c755097758f436371ada604b5cb6effcd71
Samuel Li <SamuelLi@Samuels-MacBook-Pro-2.local>
2019-09-09 12:28:45 -0400

    Design cholesterol interface

--- calculator.py
+++ calculator.py
@@ -6,17 +6,24 @@ def check_HDL(HDL_result):
 	else:
 		return "low"
 
+def cholestoral_interface():
+	print("Cholesterol check")
+	chol_input = input('Enter your cholestoral test result: ')
+	chol_data = chol_input.split("=")
+	if chol_data[0] == "HDL":
+		result = check_HDL(chol_data[1])
+		print("The result is {}".format(result))
 
 def interface():
 	print("My calculator program")
-        keep_running = True
-	        while keep_running:
-		        print("Option: ")
-		        print("0 - Quit")
-		        choice = input("Enter your choice: ")
-		        if choice == '9':
-			        return
-	        return
+    keep_running = True
+        while keep_running:
+	        print("Option: ")
+	        print("0 - Quit")
+	        choice = input("Enter your choice: ")
+	        if choice == '9':
+		        return
+        return
 
 if __name__ == "__main__":
 	interface()
